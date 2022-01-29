607bf6cd5c90563d30f3bb6f7376315f0b5cfff2
Cody Husong <codyhusong@gmail.com>
2022-01-29 12:13:40 -0600

    Added imports for the flywheel.

--- roborio/components/shooter.py
+++ roborio/components/shooter.py
@@ -1,1 +1,9 @@
 # components that are part of the shooter
+# Issue No. 6
+from ctre import (
+    WPI_TalonFX, 
+    FeedbackDevice, 
+    ControlMode, 
+    NeutralMode, 
+    TalonFXInvertType)
+    
