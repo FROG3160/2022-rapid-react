aee00433eb682f91ce016ccc0d28caff4f484b53
Hannah McCrary <hvmac6@icloud.com>
2022-01-29 11:10:00 -0600

    Added program for gyro

--- roborio/components/sensors.py
+++ roborio/components/sensors.py
@@ -1,1 +1,20 @@
 from navx import AHRS
+from magicbot import feedback
+
+class FROGGyro:
+    def __init__(self):
+      # TODO Make sure if we need this.
+        self.gyro = AHRS.create_spi()
+        self.gyro.reset()
+
+    @feedback(key='heading')
+    def getHeading(self):
+        # returns gyro heading +180 to -180 degrees
+        return self.gyro.getYaw()
+
+    def resetGyro(self):
+        # sets yaw reading to 0
+        self.gyro.reset()
+
+    def execute(self):
+        pass
