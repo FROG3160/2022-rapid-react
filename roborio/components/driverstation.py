b564279cefd61ca2eb60fa29a82be7191955b286
AbbyMacAttack <abbymac5@icloud.com>
2022-02-01 19:34:17 -0600

    added-XboxCOntroller

--- roborio/components/driverstation.py
+++ roborio/components/driverstation.py
@@ -85,3 +85,86 @@ class FROGStick(Joystick):
                 val = True
         self.update_NT('button_{}'.format(num), val)
         return val
+
+class FROGBoxSimplicity(wpilib.XboxController):
+    DEADBAND = 0.1
+    SPEED_DIVISOR = 1
+    ROTATION_DIVISOR = 1
+    DEBOUNCE_PERIOD = 0.5
+
+    def __init__(self, channel):
+
+        super().__init__(channel)
+        self.button_latest = {}
+        self.timer = wpilib.Timer
+        self.nt = NetworkTables.getTable("FROGBoxSimplicity_values")
+
+    @feedback
+    def get_speed(self):
+        speed = self.getLeftY 
+        speed = -1 * (
+            speed ** 3 / self.SPEED_DIVISOR if abs(speed) > self.DEADBAND
+            else 0
+        )
+
+    def get_debounced_button(self, num):
+        val = False
+        now = self.timer.getFPGATimestamp()
+        if self.getRawButton(num):
+            if (now - self.button_latest.get(num, 0)) > self.DEBOUNCE_PERIOD:
+                self.button_latest[num] = now
+                val = True
+        self.update_nt('button_{}'.format(num), val)
+        return val
+
+    def update_nt(self, key, value):
+        self.nt.putNumber(key, value)
+
+class FROGBoxGunner(wpilib.XboxController):
+    DEADBAND = 0.1
+    ELEVATION_DIVISOR = 1
+    ROTATION_DIVISOR = 1
+    DEBOUNCE_PERIOD = 0.5
+
+    def __init__(self, channel):
+
+        super().__init__(channel)
+        self.button_latest = {}
+        self.timer = wpilib.Timer
+        self.nt = NetworkTables.getTables("compenents/driverstation?gunner_stick")
+
+    @feedback(key='Elevation')
+    def get_elevation(self):
+        return (
+            self.getRightY / self.ELEVATION_DIVISOR
+            if abs(self.getRightX) > self.DEADBAND 
+            else 0
+        )
+
+    def get_debounced_button(self, num):
+        val = False
+        now = self.timer.getFPGATimestamp()
+        if self.getButton(num):
+             if (now - self.button_latest.get(num, 0)) > self.DEBOUNCE_PERIOD:
+                 self.button_latest[num] = now
+                 val = True
+        self.update_nt('button_{}'.format(num), val)
+        return val 
+
+    def get_debounced_POV(self):
+        val = -1
+        now = self.timer.getFPGATimestamp()
+        pov = self.getPOV()
+        if pov > -1:
+            if (now - self.button_latest.get('POV', 0)) > self.DEBOUNCE_PERIOD:
+                self.button_latest['POV'] = now
+                val = pov
+        if (now - self.button_latest.get('POV', 0)) < self.DEBOUNCE_PERIOD:
+            self.setRumble(RIGHT, 1)
+        else:
+            self.setRumble(RIGHT, 0)
+        self.update_nt('button_pov', val)
+        return val
+
+    def update_nt(self, key, value):
+        self.nt.putNumber(key, value)
