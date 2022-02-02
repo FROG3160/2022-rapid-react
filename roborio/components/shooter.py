3b0dcf21ea76ce124ad94e429ac14ca467a8e858
Hannah McCrary <hvmac6@icloud.com>
2022-02-01 19:25:57 -0600

    Create Shooter class and add Lidar Fixes #16

--- roborio/components/shooter.py
+++ roborio/components/shooter.py
@@ -1,1 +1,25 @@
 # components that are part of the shooter
+from roborio.components.sensors import FROGdar
+
+
+class FROGShooter:
+    lidar: FROGdar 
+
+    def __init__(self):
+        self.enabled = False
+
+    def enable(self):
+        self.enabled = True
+
+    def set_automatic(self):
+        pass
+
+    def set_manual(self):
+        pass
+
+    def disable(self):
+        pass
+
+    def getdistance(self):
+        # get the value/distance from the lidar in inches
+        return self.lidar.getDistance() 
