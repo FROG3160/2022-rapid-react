d2f8749973e9abecabe8db7ff6067e301bba5b9f
Hannah McCrary <hvmac6@icloud.com>
2022-02-08 19:17:02 -0600

    Changed to  led.py were made added color.

--- roborio/components/led.py
+++ roborio/components/led.py
@@ -1,17 +1,22 @@
-from ctypes.wintypes import RGB
 from ctre.led import CANdle
-from ctre import led
-
+from ctre.led import LEDStripType
 
 
 class FROGLED():
 
     def __init__(self, port : int):
-        #TODO check if CANBus id is correct
-        self.candle = CANdle(35)
-        self.candle.configLEDType(RGB)
+        self.candle = CANdle(port)
+        self.candle.configLEDType(LEDStripType.RGB)
         self.candle.setLEDs(0, 255, 0)
- 
-        
-        
-       
+
+    def ColorChangeRed(self,) :
+        self.candle.setLEDs(255, 0, 0)        
+
+    def ColorChangeBlue(self,) :
+        self.candle.setLEDs(0, 0, 255)
+
+    def WhenShoot(self,):
+        self.candle.setLEDs(252, 240, 3)
+
+    def WhenDoneShooting(self,):
+        self.candle.setLEDs(190, 252, 3)
