d1a5082d7db898a60b4b5effadd5b15bf2e3c512
Hannah McCrary <hvmac6@icloud.com>
2022-02-08 19:17:34 -0600

    Changed things in led.py added color

--- roborio/components/led.py
+++ roborio/components/led.py
@@ -1,6 +1,15 @@
+from turtle import backward, forward
 from ctre.led import CANdle
 from ctre.led import LEDStripType
+from ctre.led import StrobeAnimation, RainbowAnimation, TwinkleAnimation, ColorFlowAnimation, FireAnimation
 
+NUM_LEDS = 120
+RAINBOW = RainbowAnimation(1, 0.5, NUM_LEDS)
+STROBE = StrobeAnimation(125, 255, 3 , 0, 0.5, NUM_LEDS)
+TWIMKLE = TwinkleAnimation(0, 225, 0 , 225, 0.5, NUM_LEDS, 100)
+COLORFLOWFOWARD = ColorFlowAnimation(125,235,0,0,0.5,NUM_LEDS, forward)
+COLORFLOWBACKWARD = ColorFlowAnimation(125,235,3,0,0.5,NUM_LEDS, backward)
+FIRE = FireAnimation(1,0.5,NUM_LEDS,0.7,0.3)
 
 class FROGLED():
 
@@ -16,7 +25,7 @@ class FROGLED():
         self.candle.setLEDs(0, 0, 255)
 
     def WhenShoot(self,):
-        self.candle.setLEDs(252, 240, 3)
+        self.candle.animate(COLORFLOWFOWARD)
 
     def WhenDoneShooting(self,):
-        self.candle.setLEDs(190, 252, 3)
+        self.candle.animate(COLORFLOWBACKWARD)
