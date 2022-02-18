5901e04811167adf65f384a6245fca67a9a889e4
Brian Jarrett <celttechie@gmail.com>
2022-02-18 06:43:37 -0600

    Removed feedback wrappers causing crashes, fixed missing method call.

--- roborio/components/vision.py
+++ roborio/components/vision.py
@@ -3,7 +3,6 @@
 # Expecting 15-20 fps with cargo tracking and 50-75 fps with goal tracking.
 # Cargo cam resolution is 320x240. Goal cam is resolution is 640x480.
 
-from lib2to3.pgen2.driver import Driver
 import photonvision
 from .common import Buffer
 from wpilib import DriverStation
@@ -29,7 +28,7 @@ class FROGVision:
         # on photon vision web interface.
         self.CARGOcam = photonvision.PhotonCamera("CargoCam")
         self.Goalcam = photonvision.PhotonCamera("TargetPiCam")
-        self.driverstation = DriverStation()
+        self.driverstation = DriverStation
 
         # Sets the number of values held in each buffer.
         self.CargoYawBuff = Buffer(8)
@@ -37,7 +36,6 @@ class FROGVision:
         self.GoalYawBuff = Buffer(5)
         self.allianceColor = self.driverstation.getAlliance()
 
-    @feedback('Alliance Color')
     def getAllianceColor(self):
         return self.allianceColor
 
@@ -51,7 +49,7 @@ class FROGVision:
             return target / LC_X_div
 
     def getCargoXAverage(self):
-        if target := self.CargoYawBuff:
+        if target := self.CargoYawBuff.average():
             return target / LC_X_div
 
     def getGoalX(self):
@@ -80,7 +78,6 @@ class FROGVision:
     def setCargoRed(self):
         self.CARGOcam.setPipelineIndex(1)
 
-    @feedback("Targeted Cargo")
     def targetingAllianceCargo(self):
         return self.getCurrentCargoPipeline() == self.allianceColor + 1
 
