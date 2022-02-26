b964cfa7401bdf0c5f5d3d72dbf245e4b52328a1
Joseph Wang <joseph007wang@gmail.com>
2022-02-26 10:19:48 -0600

    controllers.py finnished as of now

--- roborio/components/controllers.py
+++ roborio/components/controllers.py
@@ -1,4 +1,3 @@
-from components.drivetrain import SwerveModule, SwerveChassis, ChassisSpeeds
 import wpilib
 from wpimath.geometry import Translation2d, Rotation2d, Pose2d
 from wpimath.kinematics import SwerveDrive4Kinematics, SwerveDrive4Odometry 
@@ -11,55 +10,54 @@ from wpimath import trajectory, controller, geometry, kinematics
 from wpimath.trajectory import constraint, TrapezoidProfileRadians, TrapezoidProfile
 import math
 from wpimath.controller import PIDController, ProfiledPIDControllerRadians, HolonomicDriveController
-
+from components.drivetrain import SwerveModule, SwerveChassis
 
 
 class trajectoryController:
+    swerveChassis: SwerveChassis
 
     
     def __init__(self):
         
         self.pose = Pose2d(Translation2d(2,2), Rotation2d(0))
-        maxVelocity = 4.96824
-        maxAcceleration = 2.48412
+        self.maxVelocity = 4.96824
+        self.maxAcceleration = 2.48412
         radianValueFor10dgrs = 0.17453293
-        Translation2d
+        self.Timer = wpilib.Timer()
+        self.Timer.start()
         
+       
+    def setup(self):
+
         # TrajectoryConfig
-        self.trajectoryConfig = trajectory.TrajectoryConfig(maxVelocity, maxAcceleration)
+        self.trajectoryConfig = trajectory.TrajectoryConfig(self.maxVelocity, self.maxAcceleration)
         
         self.trajectoryConfig.setKinematics(self.swerveChassis.kinematics)
-
-        Constraint = trajectory.TrapezoidProfileRadians.Constraints(maxVelocity, maxAcceleration)
+        
+        # Holonomic (Swerve) Controller
+        Constraint = trajectory.TrapezoidProfileRadians.Constraints(self.maxVelocity, self.maxAcceleration)
         xController = controller.PIDController(1, 0, 0, 0.02)
         yController = controller.PIDController(1, 0, 0, 0.02)
         angleController = controller.ProfiledPIDControllerRadians(1, 0, 0, Constraint, 0.02)
-        # According to the docs it says that we need all of the below methods, but the example code has only the enableContinuousInput method
         angleController.atGoal()
         angleController.atSetpoint()
         angleController.calculate(0)
         angleController.enableContinuousInput(-1, 1)
         
-
-        
         self.swerveController = controller.HolonomicDriveController(xController, yController, angleController)
-
-        self.Timer = wpilib.Timer()
-        self.Timer.start()
-
-    def setTrajectory(self):
-
-        #TrajectoryGenerate
-        self.trajectoryGenerator =  trajectory.TrajectoryGenerator.generateTrajectory(
+ 
+        # TrajectoryGenerate
+        self.trajectory1 =  trajectory.TrajectoryGenerator.generateTrajectory(
 			geometry.Pose2d(Translation2d(0, 0), geometry.Rotation2d.fromDegrees(0)), # Starting position
 			[geometry.Translation2d(2,1), geometry.Translation2d(3,2), geometry.Translation2d(2,3), geometry.Translation2d(1,2)], # Waypoints
 			geometry.Pose2d(0, 0, geometry.Rotation2d.fromDegrees(0)), # Ending position
-            self.trajectoryConfig)    
+            self.trajectoryConfig)
+  
     
+    def updatedChassisSpeeds(self):
 
-    def setChassisSpeeds(self):
-
-        sample = self.trajectoryGenerator.sample(self.Timer.get())
+        sample = self.trajectory1.sample(self.Timer.get())
         adjustedSpeeds = self.swerveController.calculate(self.swerveChassis.odometry.getPose(), sample, geometry.Rotation2d.fromDegrees(0))
         self.swerveChassis.setChassisSpeeds(adjustedSpeeds)
         
+    
